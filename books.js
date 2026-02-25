d870449622ff8f87d299a657ca1acade97ea61c4
Shyam Kommukuri <skommukuri@lifelineconnections.org>
2026-02-25 14:39:23 -0800

    Final final

--- books.js
+++ books.js
@@ -1,15 +1,17 @@
 
 
-function renderBooks(filter) {
+async function renderBooks(filter) {
     const booksWrapper = document.querySelector('.books');
 
-    const books = getBooks();
+    document.body.classList += ' books__loading'
+    const books = await getBooks();
+    document.body.classList.remove ('books__loading')
 
     if (filter === 'LOW_TO_HIGH') {
-        books.sort((a,b) => a.originalPrice - b.originalPrice);
+        books.sort((a,b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice);
     }
     else if (filter === 'HIGH_TO_LOW') {
-        books.sort((a,b) => b.originalPrice - a.originalPrice);
+        books.sort((a,b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice);
     }
     else if (filter === 'RATING') {
         books.sort((a,b) => b.rating - a.rating);
@@ -30,7 +32,7 @@ function renderBooks(filter) {
     ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
-    <span>$${book.originalPrice.toFixed(2)}</span>
+      ${priceHTML(book.originalPrice, book.salePrice)}
     </div>
     </div>`;
     })
@@ -38,6 +40,15 @@ function renderBooks(filter) {
 
     booksWrapper.innerHTML = booksHtml;
 }
+
+
+function priceHTML (originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`
+  }
+  return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span>$${salePrice.toFixed(2)}`
+}
+
 
 
 function ratingsHTML(rating) {
@@ -72,7 +83,9 @@ setTimeout(() => {
 // FAKE DATA
 
 function getBooks() {
-  return [
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
     {
       id: 1,
       title: "Crack the Coding Interview",
@@ -161,5 +174,7 @@ function getBooks() {
       salePrice: null,
       rating: 4.5,
     },
-  ];
+      ]);
+    }, 500);
+  });
 }
