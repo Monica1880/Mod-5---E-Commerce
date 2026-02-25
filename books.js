cdb71d10bd8783da22d6978ba951b0f2c34c0b78
Shyam Kommukuri <skommukuri@lifelineconnections.org>
2026-02-24 21:45:47 -0800

    3

--- books.js
+++ books.js
@@ -1,19 +1,32 @@
 
 
-function renderBooks() {
+function renderBooks(filter) {
     const booksWrapper = document.querySelector('.books');
 
     const books = getBooks();
-    console.log(books)
-    booksWrapper.innerHTML =
-    `<div class="book">
+
+    if (filter === 'LOW_TO_HIGH') {
+        books.sort((a,b) => a.originalPrice - b.originalPrice);
+    }
+    else if (filter === 'HIGH_TO_LOW') {
+        books.sort((a,b) => b.originalPrice - a.originalPrice);
+    }
+    else if (filter === 'RATING') {
+        books.sort((a,b) => b.rating - a.rating);
+    }
+
+
+
+    const booksHtml = books.map((book) => {
+        return `<div class="book">
     <figure class="book__img--wrapper">
-    <img class="book__img" src="${books[0].url}" alt="">
+    <img class="book__img" src="${book.url}" alt="">
     </figure>
     <div class="book__title">
-    ${books[0].title}
+    ${book.title}
     </div>
     <div class="book__ratings">
+    ${ratingsHTML(book.rating)}
     <i class="fas fa-star"></i>
     <i class="fas fa-star"></i>
     <i class="fas fa-star"></i>
@@ -21,14 +34,41 @@ function renderBooks() {
     <i class="fas fa-star-half-alt"></i>
     </div>
     <div class="book__price">
-    <span class="book__price--normal">$59.95</span> $14.95
+    <span>$${book.originalPrice.toFixed(2)}</span>
     </div>
     </div>`
+    })
+    .join("");
+
+    booksWrapper.innerHTML = booksHtml;
 }
+
+
+function ratingsHTML(rating) {
+     let ratingHTML = "";  
+    for (let i = 0; i < Math.floor(rating); ++i) {
+        ratingHTML += '<i class="fas fa-star"></i>\n';
+    }
+    if (!Number.isInteger(rating)) {
+        ratingHTML += '<i class="fas fa-star-half-alt"></i>\n';
+    }
+    return ratingsHTML;
+}    
+
+
+
+function filterBooks(event) {
+    renderBooks(event.target.value);
+}
+
+
 
 setTimeout(() => {
     renderBooks();
 });
+
+
+
 
 
 // ************************************************
